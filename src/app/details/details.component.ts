02b956daf185df13fbc16de157e683a54df2c263
Rodrigo Blefari Gonçalves <rodrigo.blefari@gmail.com>
2024-02-22 15:48:21 -0300

    feat: services with mock and full router solution. and details component e home component getting service

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,19 +1,47 @@
 import { Component, inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ActivatedRoute } from "@angular/router";
+import { HousingService } from "../housing.service";
+import { Housinglocation } from "../housing-location/housinglocation";
 
 @Component({
   selector: "app-details",
   standalone: true,
   imports: [CommonModule],
-  template: ` <p>details works! id = {{ housingLocationId }}</p> `,
+  template: `
+    <article>
+      <img [src]="housingLocation?.photo" alt="" class="listing-photo" />
+      <section class="listing-description">
+        <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
+        <p class="listing-location">
+          {{ housingLocation?.city }}, {{ housingLocation?.state }}
+        </p>
+      </section>
+      <section class="list-features">
+        <h2 class="section-heading">About this housing location</h2>
+        <ul>
+          <li>Units available: {{ housingLocation?.availableUnits }}</li>
+          <li>Wifi available: {{ housingLocation?.wifi }}</li>
+          <li>laundry available: {{ housingLocation?.laundry }}</li>
+        </ul>
+      </section>
+      <section class="listing-apply">
+        <h2 class="section-heading">Aply now to live here</h2>
+        <button type="button" class="primary"> Aply Now!</button>
+      </section>
+    </article>
+  `,
   styleUrls: ["./details.component.css"],
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
-  housingLocationId: number = 0;
+  housingService: HousingService = inject(HousingService);
+  housingLocation!: Housinglocation | undefined;
 
   constructor() {
-    this.housingLocationId = +this.route.snapshot.params["id"];
+    const housingLocationId = +this.route.snapshot.params["id"];
+    this.housingLocation =
+      this.housingService.getHousingLocationById(housingLocationId);
+    console.log(this.housingLocation);
   }
 }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,16 +1,22 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { DetailsComponent } from "./details/details.component";
+
 const routeConfig: Routes = [
   {
     path: "",
     component: HomeComponent,
-    title: "Home Page",
+    pathMatch: "full", // Certifique-se de que esta rota seja acessada somente quando o caminho for exatamente vazio
+    data: { title: "Home Page" }, // Adicione dados adicionais à rota, se necessário
   },
   {
     path: "details/:id",
     component: DetailsComponent,
-    title: "Details Page",
+    data: { title: "Details Page" }, // Adicione dados adicionais à rota, se necessário
+  },
+  {
+    path: "**", // Rota curinga para capturar rotas não encontradas
+    redirectTo: "", // Redireciona para a página inicial caso a rota não seja encontrada
   },
 ];
 
